e6344f241a4638fcd43a33e8c67e850b30cfcac3
Alexey Kuznetsov <tarantyl603@mail.ru>
2022-05-28 17:18:55 +0300

    Revie_Oshibka_sqlite

--- docker/script/init.sh
+++ docker/script/init.sh
@@ -5,8 +5,14 @@ sleep 10
 airflow upgradedb
 sleep 10
 
-#Создаем пользователя
-airflow users create --username admin --password admin --firstname Alexey --lastname Kuznetsov --role Admin --email kuznetsovlearn@gmail.com
-
 # Запуск шедулера и вебсервера
 airflow scheduler & airflow webserver
+
+#Create User Airflow
+airflow users create \
+    --username admin \
+    --password admin \
+    --firstname Alexey \
+    --lastname Kuznetsov \
+    --role Admin \
+    --email kuznetsovlearn@gmail.com
