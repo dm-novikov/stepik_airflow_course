1a405db05020d07e488a99efb5d062879c5cf70a
Alexey Kuznetsov <tarantyl603@mail.ru>
2022-06-01 23:43:11 +0300

    Zadanie vipolneno

--- docker/script/init.sh
+++ docker/script/init.sh
@@ -5,14 +5,8 @@ sleep 10
 airflow upgradedb
 sleep 10
 
+#создание пользователя
+python /project/scripts/create_user.py
+
 # Запуск шедулера и вебсервера
 airflow scheduler & airflow webserver
-
-#Create User Airflow
-airflow users create \
-    --username admin \
-    --password admin \
-    --firstname Alexey \
-    --lastname Kuznetsov \
-    --role Admin \
-    --email kuznetsovlearn@gmail.com
